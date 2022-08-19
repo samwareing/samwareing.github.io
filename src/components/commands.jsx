39dbff069af1144a788a8a15de0187638f1ce323
samwareing <98537672+samwareing@users.noreply.github.com>
2022-08-19 14:18:18 +0100

    Refactor commands.
    Commands now exist in a single file.
    
    Commands are represented as functions that return arrays with jsx.

---
+++ src/components/commands.jsx
@@ -0,0 +1,266 @@
+const commands = {
+  about: (key) => {
+    return [
+      "Sam Wareing",
+      <br key={key + 1} />,
+      "Computational Physicist",
+      <br key={key + 2} />,
+      "Reading, UK",
+      <br key={key + 3} />,
+      "Highly trained and motivated software developer with a background in physics. Four years of experience in software development including back end physics simulation, GUI development, full stack web development and database management. Aspirations to branch out into other areas of software engineering. Efficient self learner. Works well in a team.",
+      <br key={key + 3} />,
+    ];
+  },
+
+  clear: (key) => {
+    return [""];
+  },
+
+  // cv: (key) => {
+  //   return ["If you would like to download my CV as a .pdf click here."];
+  // },
+
+  education: (key) => {
+    return [
+      "Physics and Technology of Nuclear Reactors MSc. - University of Birmingham",
+      <br key={key + 1} />,
+      "  Sep 2017 - Dec 2018 - Birmingham, UK",
+      <br key={key + 2} />,
+      "  Distinction",
+      <br key={key + 3} />,
+      "  School of Physics and Astronomy",
+      <br key={key + 4} />,
+      <br key={key + 5} />,
+      "Nuclear Science and Materials BSc.- University of Birmingham",
+      <br key={key + 6} />,
+      "  Sep 2014 - Jun 2017 - Birmingham, UK",
+      <br key={key + 7} />,
+      "  2:1",
+      <br key={key + 8} />,
+      "  School of Metallurgy and Materials",
+      <br key={key + 9} />,
+    ];
+  },
+
+  email: (key) => {
+    return [
+      "Email me at: ",
+      <a href="mailto:samwareing@outlook.com" key={key + 1}>
+        samwareing@outlook.com
+      </a>,
+    ];
+  },
+
+  experience: (key) => {
+    return [
+      "Defence Contractor - Computational Physicist",
+      <br key={key + 1} />,
+      "  Oct 2020 – Current, Reading, UK",
+      <br key={key + 2} />,
+      "  Confidential work on GUI development (Python, Tkinter) and physics simulation (Python and C++). Recent experience in database management (SQL/MariaDB) and website development (PHP and JavaScript). Working in a collaborative agile software development team (SCRUMBAN), with developers at home in the UK and the United States. Software development work on the implementation of a new Monte Carlo gamma-electron physics simulation code (C++), with  GPU architecture capability. Software development work extending an existing deterministic radiation transport code, creating and implementing new simulation algorithms (Python), as well as developing a new GUI for it (Python, Tkinter)",
+      <br key={key + 3} />,
+      <br key={key + 4} />,
+      "Defence Contractor - Graduate Physicist",
+      <br key={key + 5} />,
+      "  Sep 2018 – Oct 2020, Reading, UK",
+      <br key={key + 6} />,
+      "  A highly competitive graduate scheme role that included multiple placements in different departments throughout the company. Scientific research work producing scientific technical papers. Software development placements leading towards my current role as a computational physicist. This graduate scheme involved a project management placement to produce an in house statistics technical training module.",
+      <br key={key + 7} />,
+      <br key={key + 8} />,
+      "Culham Centre for Fusion Energy (CCFE) - Industrial project",
+      <br key={key + 9} />,
+      "  Jun 2018 – Sep 2018, Abingdon, UK",
+      <br key={key + 10} />,
+      "  An industrial research project working for CCFE. This masters project was an invaluable first experience of software development for physics simulation codes. Software development work on the integration of CCFE's MCR2S code (F95) with the Monte Carlo Reactor Physics code Serpent (C). This work is used to calculate gamma ray dose levels of a nuclear fusion reactor post irradiation, in order to assess radiation risk to reactor maintenance workers.",
+      <br key={key + 11} />,
+    ];
+  },
+
+  github: (key) => {
+    return [
+      "Check out my github at: ",
+      <a
+        href="https://github.com/samwareing"
+        key={key + 1}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        https://github.com/samwareing
+      </a>,
+    ];
+  },
+
+  help: (key) => {
+    return [
+      "Help:",
+      <br key={key + 1} />,
+      "  Type one of the following commands and press enter:",
+      <br key={key + 2} />,
+      '    "about" : prints a message about me',
+      <br key={key + 3} />,
+      '    "clear" : clears the display',
+      // <br key={key + 4} />,
+      // '    "cv" : prints a link to download my curriculum vitae',
+      <br key={key + 5} />,
+      '    "education" : prints a message about my education',
+      <br key={key + 6} />,
+      '    "email" : prints my email address',
+      <br key={key + 7} />,
+      '    "experience" : prints a message about my career experience',
+      <br key={key + 8} />,
+      '    "github" : prints my github site',
+      <br key={key + 9} />,
+      '    "help" : prints this help message',
+      <br key={key + 10} />,
+      '    "misc" : prints a message about me as a person',
+      <br key={key + 11} />,
+      '    "phone" : prints my phone number',
+      <br key={key + 12} />,
+      '    "references" : prints a message about my references',
+      <br key={key + 13} />,
+      '    "skills" : prints a message about my developer skills',
+      <br key={key + 14} />,
+      '    "training" : prints a message about my professional training qualifications',
+      <br key={key + 15} />,
+    ];
+  },
+
+  misc: (key) => {
+    return [
+      "Music lover",
+      <br key={key + 1} />,
+      "15+ years of experience with the guitar and the piano",
+      <br key={key + 2} />,
+      "Plays in an indie rock band ",
+      <br key={key + 3} />,
+      "Enjoys the gym",
+      <br key={key + 4} />,
+      "Full UK drivers license",
+    ];
+  },
+
+  phone: (key) => {
+    return [
+      "Phone me at: ",
+      <a href="tel:+447903633897" key={key + 1}>
+        +44 (0)7903 633897
+      </a>,
+    ];
+  },
+
+  references: (key) => {
+    return ['References are available upon request, try entering "email".'];
+  },
+
+  skills: (key) => {
+    return [
+      "Programming Languages:",
+      <br key={key + 1} />,
+      "  Experienced:",
+      <br key={key + 2} />,
+      "    Python, C/C++",
+      <br key={key + 3} />,
+      "  Touched on:",
+      <br key={key + 4} />,
+      "    SQL, HTML5, CSS3, JavaScript, PHP, Bash, F77/F95, R",
+      <br key={key + 5} />,
+      <br key={key + 6} />,
+      "Libraries:",
+      <br key={key + 7} />,
+      "  Experienced:",
+      <br key={key + 8} />,
+      "    Tkinter",
+      <br key={key + 9} />,
+      "  Touched on:",
+      <br key={key + 10} />,
+      "    Django, Pandas, Numpy, Bootstrap5, Font Awesome",
+      <br key={key + 11} />,
+      "  Currently learning:",
+      <br key={key + 12} />,
+      "    ReactJS",
+      <br key={key + 13} />,
+      <br key={key + 14} />,
+      "Miscellaneous developer skills:",
+      <br key={key + 15} />,
+      "  Experienced:",
+      <br key={key + 16} />,
+      "    Git, Unix, TDD, SCRUMBAN",
+      <br key={key + 17} />,
+      "  Touched on:",
+      <br key={key + 18} />,
+      "    DevOps",
+      <br key={key + 19} />,
+      "  Currently learning:",
+      <br key={key + 20} />,
+      "    Docker, Basecamp's Shape Up Method",
+      <br key={key + 21} />,
+    ];
+  },
+
+  training: (key) => {
+    return [
+      "Learning Tree International - London, UK and remote:",
+      <br key={key + 1} />,
+      "  HTML5, CSS3 and JavaScript: Platform-Independent Web Development - Feb 2022",
+      <br key={key + 2} />,
+      "  Introduction to SQL Programming - Dec 2021",
+      <br key={key + 3} />,
+      "  Introduction to Data Science, Machine Learning & AI using Python - Mar 2021",
+      <br key={key + 4} />,
+      "  DevOps Foundation® (DOFD) - Nov 2020",
+      <br key={key + 5} />,
+      "  Advanced Python: Best Practices and Design Patterns - Apr 2020",
+      <br key={key + 6} />,
+      "  Agile Fundamentals - Sep 2019",
+      <br key={key + 7} />,
+      "  Introduction to Python - Aug 2019",
+      <br key={key + 8} />,
+      <br key={key + 9} />,
+      "JB International - London, UK:",
+      <br key={key + 10} />,
+      "  C++ Advanced, Modern C++11/14/17 - May 2019",
+      <br key={key + 11} />,
+      "  C++ Introduction - March 2019",
+      <br key={key + 12} />,
+    ];
+  },
+
+  hello: (key) => {
+    return ['Hi. Try entering "help".'];
+  },
+  goodbye: (key) => {
+    return ["No, please don't go!"];
+  },
+  more: (key) => {
+    return ["Don't be greedy."];
+  },
+  less: (key) => {
+    return ["Less is sometimes more."];
+  },
+  pwd: (key) => {
+    return ["You're right where you need to be."];
+  },
+  ls: (key) => {
+    return ["Looks like there's nothing here."];
+  },
+  dir: (key) => {
+    return ["Looks like there's nothing here."];
+  },
+  git: (key) => {
+    return ["How rude!"];
+  },
+  cp: (key) => {
+    return ["Now why would you want to do that?"];
+  },
+  mv: (key) => {
+    return ["I don't think so."];
+  },
+  rm: (key) => {
+    return ["I really don't think so."];
+  },
+  python: (key) => {
+    return ["I really really don't think so."];
+  },
+};
+
+export { commands };
--- src/components/terminalBox.jsx
+++ src/components/terminalBox.jsx
@@ -4,10 +4,9 @@ import TerminalMenuBar from "./terminalMenuBar";
 import TerminalText from "./terminalText";
 import TerminalInput from "./terminalInput";
 
-const stringCommands = require("../data/stringCommands.json");
-const allowedStringCommands = Object.keys(stringCommands);
+import { commands } from "./commands";
 
-const allowedActionCommands = ["email", "phone"];
+const allowedCommands = Object.keys(commands);
 
 function TerminalBox() {
   const [lines, setLinesArray] = useState([]);
@@ -26,39 +25,8 @@ function TerminalBox() {
     ]);
   };
 
-  const addAllowedStringCommandValue = (enteredCommand) => {
-    const split = stringCommands[enteredCommand].split("\n");
-
-    for (let key in split) {
-      setLinesArray((lines) => [
-        ...lines,
-        split[key],
-        <br key={lines.length} />,
-      ]);
-    }
-  };
-
-  const addAllowedActionCommandValue = (enteredCommand) => {
-    let htmlElements = [];
-    if (enteredCommand === "email") {
-      htmlElements.push("Email me at: ");
-      htmlElements.push(
-        <a
-          href="mailto:samwareing@outlook.com"
-          key={lines.length + htmlElements.length}
-        >
-          samwareing@outlook.com
-        </a>
-      );
-    } else if (enteredCommand === "phone") {
-      htmlElements.push("Phone me at: ");
-      htmlElements.push(
-        <a href="tel:+447903633897" key={lines.length + htmlElements.length}>
-          +44 (0)7903 633897
-        </a>
-      );
-    }
-
+  const addCommandValue = (enteredCommand) => {
+    let htmlElements = commands[enteredCommand](lines.length);
     setLinesArray((lines) => [
       ...lines,
       ...htmlElements,
@@ -91,10 +59,8 @@ function TerminalBox() {
       return;
     }
 
-    if (allowedStringCommands.includes(enteredCommand)) {
-      addAllowedStringCommandValue(enteredCommand);
-    } else if (allowedActionCommands.includes(enteredCommand)) {
-      addAllowedActionCommandValue(enteredCommand);
+    if (allowedCommands.includes(enteredCommand)) {
+      addCommandValue(enteredCommand);
     } else {
       addErrorString(enteredCommand);
     }
