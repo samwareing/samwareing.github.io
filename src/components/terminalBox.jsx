0ffd5226d1e67d08526630c2f2925f34ca48cc52
samwareing <98537672+samwareing@users.noreply.github.com>
2022-08-19 11:54:44 +0100

    Implement links to print.
    This can be refactored.
    
    Rename commandActions to stringCommands, and removed phone and email from the json.

--- src/components/terminalBox.jsx
+++ src/components/terminalBox.jsx
@@ -4,8 +4,10 @@ import TerminalMenuBar from "./terminalMenuBar";
 import TerminalText from "./terminalText";
 import TerminalInput from "./terminalInput";
 
-const commandActions = require("../data/commandActions.json");
-let allowedCommands = Object.keys(commandActions);
+const stringCommands = require("../data/stringCommands.json");
+const allowedStringCommands = Object.keys(stringCommands);
+
+const allowedActionCommands = ["email", "phone"];
 
 function TerminalBox() {
   const [lines, setLinesArray] = useState([]);
@@ -24,8 +26,8 @@ function TerminalBox() {
     ]);
   };
 
-  const addAllowedCommandValue = (enteredCommand) => {
-    const split = commandActions[enteredCommand].split("\n");
+  const addAllowedStringCommandValue = (enteredCommand) => {
+    const split = stringCommands[enteredCommand].split("\n");
 
     for (let key in split) {
       setLinesArray((lines) => [
@@ -34,6 +36,34 @@ function TerminalBox() {
         <br key={lines.length} />,
       ]);
     }
+  };
+
+  const addAllowedActionCommandValue = (enteredCommand) => {
+    let htmlElements = [];
+    if (enteredCommand === "email") {
+      htmlElements.push("Email me at: ");
+      htmlElements.push(
+        <a
+          href="mailto:samwareing@outlook.com"
+          key={lines.length + htmlElements.length}
+        >
+          samwareing@outlook.com
+        </a>
+      );
+    } else if (enteredCommand === "phone") {
+      htmlElements.push("Phone me at: ");
+      htmlElements.push(
+        <a href="tel:+447903633897" key={lines.length + htmlElements.length}>
+          +44 (0)7903 633897
+        </a>
+      );
+    }
+
+    setLinesArray((lines) => [
+      ...lines,
+      ...htmlElements,
+      <br key={lines.length + htmlElements.length} />,
+    ]);
   };
 
   const addErrorString = (enteredCommand) => {
@@ -61,8 +91,10 @@ function TerminalBox() {
       return;
     }
 
-    if (allowedCommands.includes(enteredCommand)) {
-      addAllowedCommandValue(enteredCommand);
+    if (allowedStringCommands.includes(enteredCommand)) {
+      addAllowedStringCommandValue(enteredCommand);
+    } else if (allowedActionCommands.includes(enteredCommand)) {
+      addAllowedActionCommandValue(enteredCommand);
     } else {
       addErrorString(enteredCommand);
     }
